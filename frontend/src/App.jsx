b98540893d0c593b18354e3af20b03ab1adb8d73
Yuvraj Negi <yuvnegiraj2101@gmail.com>
2026-06-25 17:52:53 +0530

    Remove UiShowcase route from App component
    Removed the UiShowcase route from the application.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,6 @@ import Home from "./pages/Home";
 import Classify from "./pages/Classify";
 import History from "./pages/History";
 import About from "./pages/About";
-import UiShowcase from "./pages/UiShowcase";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
@@ -27,7 +26,6 @@ function Gate() {
         <Route path="/classify" element={<Classify />} />
         <Route path="/history" element={<History />} />
         <Route path="/about" element={<About />} />
-        <Route path="/ui-showcase" element={<UiShowcase />} />
       </Routes>
     </BrowserRouter>
   );
